1cc826c9b0289bc5c3b7dad1eaca92b1b5ea9b66
ArunArivanandam <arunarivanandam@gmail.com>
2024-03-13 18:24:57 +0530

    add profanity checker

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -71,11 +74,47 @@ func handlerQuotesValidate(w http.ResponseWriter, r *http.Request) {
 		params.Author = "unknown"
 	}
 
+	badWords := map[string]struct{}{
+		"fuck":   {},
+		"shit":   {},
+		"badass": {},
+		"dick":   {},
+		"sex":    {},
+	}
+
+	cleaned := getCleanedQuote(params.Quote, badWords)
+
 	respondWithJSON(w, http.StatusOK, parameters{
-		Quote:  params.Quote,
+		Quote:  cleaned,
 		Author: params.Author,
 	})
 
+}
+
+func getCleanedQuote(quote string, badWords map[string]struct{}) string {
+	words := strings.Split(quote, " ")
+	fmt.Println(words)
+	for i, word := range words {
+		if word[len(word)-1] == ',' {
+			newWord := word[0 : len(word)-1]
+			lowWord := strings.ToLower(newWord)
+			if _, ok := badWords[lowWord]; ok {
+				re := regexp.MustCompile("[a-zA-Z]")
+				replacedStr := re.ReplaceAllString(string(lowWord[2:]), "*")
+				words[i] = string(lowWord[0]) + replacedStr + string(lowWord[len(lowWord)-1:]) + ","
+			}
+
+		}
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			re := regexp.MustCompile("[a-zA-Z]")
+			replacedStr := re.ReplaceAllString(string(loweredWord[2:]), "*")
+			words[i] = string(loweredWord[0]) + replacedStr + string(loweredWord[len(loweredWord)-1:])
+		}
+	}
+
+	cleaned := strings.Join(words, " ")
+	return cleaned
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
